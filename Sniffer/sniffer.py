1370983698753f8cdd9a89e40839a05eaae52055
James <james.woolley5@googlemail.com>
2013-06-05 20:34:08 +0200

    Update sniffer.py
    working version

--- Sniffer/sniffer.py
+++ Sniffer/sniffer.py
@@ -1,13 +1,12 @@
-import time
 import subprocess
-from subprocess import call
-from datetime import datetime
-fileName = str(datetime.now().day) + "-" + str(datetime.now().month) + "-" + str(datetime.now().year) + " AT " + str(datetime.now().hour) + "-" + str(datetime.now().minute)
+from dbupload import upload_file #Used for Dropbox uploading
+from datetime import datetime # Used the genreate the filename
+count = 0 #Counts the number of files that have been dumped
 while True:
-    tcpDumpProcess = subprocess.call(["tcpdump", "-w", fileName, "-i", "eth0"])
-    currentHour = datetime.now().minute
-    while True:
-        if datetime.now().minute != currentHour:
-            tcpDumpProcess.terminate()
-            print "CHJAMNGE"
-            break
+    count = count + 1
+    fileName = str(datetime.now().day) + "-" + str(datetime.now().month) + "-" + str(datetime.now().year) + " AT " + str(datetime.now().hour) + "-" + str(datetime.now().minute)
+    tcpDumpProcess = subprocess.Popen(["tcpdump", "-Z", "root", "-w", fileName, "-i", "bridge0", "-G", "60", "-W", "1"]) #Sets up the TCPDump command
+    tcpDumpProcess.communicate() #Runs the TCPDump command
+    print "Currently dumping file number " + str(count) + "."
+    upload_file(fileName,"/",fileName, "YOUR_EMAIL","YOUR_PASSWORD") #Uploads the dump file to dropbox
+    print "File uploaded Successfully"
